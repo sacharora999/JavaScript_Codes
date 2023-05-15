f35e6569bf01f26f756e37b70bd42daceff6abc4
Sachin Arora <sacharora999@gmail.com>
2023-05-15 15:02:59 +0000

    JS Switch Case

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+const expr = "Apple"
+
+switch (expr) {
+  case "Applee":
+    console.log("Expression Apple")
+    break;
+  case "Mango":
+    console.log("Expression Mango")
+    break;
+  default:
+    console.log("Expression Default")
+
+}
+
+a = 20
+
+console.log("You can ", (a < 18 ? "NOT Drive" : "Drive"))
